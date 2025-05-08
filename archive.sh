cc6c3d35c2b9c2f8c352cac063f51a764df993f8
Eric Zhao <21zhaoe@protonmail.com>
2025-05-08 16:33:57 -0400

    stuff

--- archive.sh
+++ archive.sh
@@ -4,10 +4,10 @@ set -e
 # build archive
 rm -f archive.zip
 zip -r archive.zip \
-  input src \
+  input src Cargo.toml Cargo.lock \
   requirements.txt \
   compile.sh run.sh runAll.sh \
   report.pdf \
   team.txt \
   results.log \
-  slides.pdf
+  presentation.pdf
